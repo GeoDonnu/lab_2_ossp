7a9ef8b5c0be961826e820aacb471b9f6d41aa51
GeoDonny <prihodskij.georgij@gmail.com>
2025-03-19 18:29:12 +0000

    Initial commit

---
+++ init_repo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Perevirka parametriv
+
+if ["$#" -ne 2]; then
+	echo "Pomilka: neobxidno vkazatu dva parametri."
+	echo "Vikoristannya: $0 <shlyax_katalog> <shlyax_repo>"
+	exit 1
+fi
+
+#Priyom parametriv
+
+LOCAL_DIR = $1
+REMOTE_REPO = $2
+
+#Zapit login ta parol
+
+read -p "Vvedit vash GitHub login: " GITHUB_USER
+read -s -p "Vvedit vash GitHub parol: " GITHUB_TOKEN
+echo ""
+
+#Perexid y vkazaniy katalog
+
+cd "$LOCAL_DIR" || {echo "Pomylka: Ne vdalosya pereyti v katalog $LOCAL_DIR "
+
+
+#init repo
+
+git init
+
+#Dodavannya viddalenogo repository
+
+git remote add origin "https://$GITHUB_USER:$GITHUB_TOKEN@github.com/REMOTE_REPO.git"
+
+#dodavannya vsix files in repo
+
+git add .
+
+git commit -m "Initial commit"
+
+git push -u origin main
+
+echo "Code yspishno zavantazheno y repository"
+
